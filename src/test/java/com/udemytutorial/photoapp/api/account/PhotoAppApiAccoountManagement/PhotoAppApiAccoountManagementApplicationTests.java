66a1b44d384bbb2380286867ff95549e5ddf9c85
Rupesh <rupeshkumar9312@gmail.com>
2021-02-26 00:32:34 -0800

    wip account management service

---
+++ src/test/java/com/udemytutorial/photoapp/api/account/PhotoAppApiAccoountManagement/PhotoAppApiAccoountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemytutorial.photoapp.api.account.PhotoAppApiAccoountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccoountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
